f9b55de1e9029a3bfe573ca3b8c73658922348c8
VeronicaOtherWorld <aisileyi@gmail.com>
2025-03-21 15:33:23 +0000

    add navvar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div
+      className="fixed top-0 left-0 w-full bg-white shadow-md
+     h-20 flex items-center px-4 md:px-8 z-50"
+    >
+      {/* logo part */}
+      <h1 className="text-xl font-bold text-green-600">UrbanGrow</h1>
+      {/* menus */}
+      <nav className="hidden md:flex gap-6 absolute left-1/2 transform -translate-x-1/2">
+        <Link to="/" className="text-gray-700 hover:text-green-600">
+          Home
+        </Link>
+        <Link to="/diagnosis" className="text-gray-700 hover:text-green-600">
+          Diagnosis
+        </Link>
+        <Link to="/products" className="text-gray-700 hover:text-green-600">
+          Products
+        </Link>
+        <Link to="/services" className="text-gray-700 hover:text-green-600">
+          Services
+        </Link>
+        <Link to="/community" className="text-gray-700 hover:text-green-600">
+          Community
+        </Link>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/layouts/TabLayout.jsx
+++ src/layouts/TabLayout.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import BottomTab from "../components/BottomTab";
-
+import Navbar from "../components/Navbar";
 const TabLayout = () => {
   const location = useLocation();
   return (
     <div className="min-h-screen max-w-[430px] mx-auto flex flex-col">
       <div className="flex-1 overflow-y-auto pb-16">
-        <Outlet />
+        {/* top navbar */}
+        <Navbar></Navbar>
+        {/* outlet parts */}
+        <div className="flex-1 mt-20 overflow-y-auto">
+          <Outlet />
+        </div>
       </div>
+      {/* bottom tabs */}
       <BottomTab currentPath={location.pathname} />
     </div>
   );
